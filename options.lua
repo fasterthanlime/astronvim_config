575c7d05a78fdf9c17d0c552c5c0a6826a241d0d
Amos Wenger <amos@bearcove.net>
2023-07-04 12:17:19 +0200

    Use system clipboard

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
     wrap = false,          -- sets vim.opt.wrap
     colorcolumn = "80",
+    clipboard = "unnamedplus",
     -- textwidth = 80,
   },
   g = {
