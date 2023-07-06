d85108adc15bd8163919720f4aebae8daa8c78b1
Amos Wenger <amos@bearcove.net>
2023-07-06 17:48:19 +0200

    Use dayfox theme

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  colorscheme = "dayfox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
